9c958eb4da2426e8beef4261aec4fb4e7ba3feac
Koleok <kchamb3@gmail.com>
2016-06-04 02:35:49 -0400

    Convert to js

---
+++ examples/hello.js
@@ -0,0 +1,19 @@
+const MongoCache = require('../lib')
+
+const cache = new MongoCache({
+  collection: 'cache',
+  db: 'test',
+  host: 'localhost',
+  pass: '',
+  port: 27017,
+  user: 'admin'
+});
+
+cache.set('hello', 'world', 10, (err, value) => {
+  console.log('set', err, value);
+
+  return cache.get('hello', (err, value) => {
+    console.log('get', err, value);
+    return process.exit(0);
+  });
+});
